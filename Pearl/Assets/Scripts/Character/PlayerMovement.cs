fc26742d704e77ce6b92823f1ef77ce61b2e7b51
Park Hyunho <lumenize@ajou.ac.kr>
2023-07-04 14:11:48 +0900

    add Joystick Movement

---
+++ Pearl/Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static PlayerMovement Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<PlayerMovement>();
+                if (instance == null)
+                {
+                    var instanceContainer = new GameObject("PlayerMovement");
+                    instance = instanceContainer.AddComponent<PlayerMovement>();
+                }
+            }
+            return instance;
+        }
+    }
+    private static PlayerMovement instance;
+
+    Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        if (JoystickMovement.Instance.joyVec.x != 0 || JoystickMovement.Instance.joyVec.y != 0)
+        {
+            rb.velocity = new Vector3(JoystickMovement.Instance.joyVec.x, JoystickMovement.Instance.joyVec.y,0)*moveSpeed;
+        }
+        else//x,y 둘다 0 일때 멈추기
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Pearl/Assets/Scripts/UI/JoystickMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickMovement : MonoBehaviour
+{
+    public static JoystickMovement Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<JoystickMovement>();
+                if (instance == null)
+                {
+                    var instanceContainer = new GameObject("JoystickMovement");
+                    instance = instanceContainer.AddComponent<JoystickMovement>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private static JoystickMovement instance;
+
+    public GameObject smallStick;
+    public GameObject bGStick;
+    Vector3 stickFirstPosition;
+    public Vector3 joyVec;
+    float stickRadius;
+    void Start()
+    {
+        stickRadius = bGStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+    }
+
+    public void PointDown()
+    {
+        bGStick.SetActive(true);
+        bGStick.transform.position = Input.mousePosition;
+        smallStick.transform.position = Input.mousePosition;
+        stickFirstPosition= Input.mousePosition;
+    }
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3 DragPosition = pointerEventData.position;
+        joyVec = (DragPosition - stickFirstPosition).normalized;
+        float stickDistacne = Vector3.Distance(DragPosition, stickFirstPosition);
+
+        if (stickDistacne < stickRadius)
+        {
+            smallStick.transform.position = stickFirstPosition+joyVec*stickDistacne;
+        }
+        else
+        {
+            smallStick.transform.position = stickFirstPosition+joyVec*stickRadius;
+        }
+    }
+
+    public void Drop()
+    {
+        joyVec = Vector3.zero;
+        bGStick.SetActive(false);
+    }
+    
+    
+}
