a888522458713de91ffafa706672aadea6c23ec8
Seok6704 <dnflrkcjswo@naver.com>
2023-07-14 16:22:25 +0900

    IdleEnemy script Update
    IdleEnemy 스크립트 변수를 인스펙터 창에서 조절 가능하게 수정하였습니다.

--- Pearl/Assets/Scripts/Enemy/IdleEnemy.cs
+++ Pearl/Assets/Scripts/Enemy/IdleEnemy.cs
@@ -10,12 +10,18 @@ using TMPro;
 
 public class IdleEnemy : MonoBehaviour
 {
+    public EnemyData[] enemyData;
+
     CharacterBase stat; // 캐릭터 스탯
     IngameGoods goods; // 현재 재화
+
+    [Header ("시작 스테이지 (1-1~4 = 1~4, 2-1~4 = 5~8, 3-1~4 = 9~12)")]
     public int nowStage; // 현재 스테이지 (1-1~4 = 1~4, 2-1~4 = 5~8, 3-1~4 = 9~12)
+    [Header ("시작 챕터(영향 X)")]
     public int nowChapter; //현재 챕터 (1, 2, 3)
-    public int enemyHp, enemyAtk, enemyRapid; // 상대 체력, 공격력, 공격속도
-    public uint rewardGold, rewardManaStone; // 보상 골드, 보상 마력석
+
+    int enemyHp, enemyAtk, enemyRapid; // 상대 체력, 공격력, 공격속도
+    uint rewardGold, rewardManaStone; // 보상 골드, 보상 마력석*/
     public TextMeshProUGUI nowStageText;
     float nowDmg; // 현재 공격력
     int nowHp; // 현재 체력
@@ -83,32 +89,57 @@ public class IdleEnemy : MonoBehaviour
         switch(stage)
         {
             case 1:
-                enemyHp = 1000;
+                /*enemyHp = 1000;
                 enemyAtk = 10;
                 enemyRapid = 1;
                 rewardGold = 10;
-                rewardManaStone = 1;
+                rewardManaStone = 1;*/
+                enemyHp = enemyData[0].enemyHp;
+                enemyAtk = enemyData[0].enemyAtk;
+                enemyRapid = enemyData[0].enemyRapid;
+                rewardGold = enemyData[0].rewardGold;
+                rewardManaStone = enemyData[0].rewardManaStone;
                 nowStageText.text = "1 - 1";
                 nowChapter = 1;
                 break;
             case 2:
-                enemyHp = 3000;
+                /*enemyHp = 3000;
                 enemyAtk = 50;
                 enemyRapid = 1;
                 rewardGold = 100;
-                rewardManaStone = 10;
+                rewardManaStone = 10;*/
+                enemyHp = enemyData[1].enemyHp;
+                enemyAtk = enemyData[1].enemyAtk;
+                enemyRapid = enemyData[1].enemyRapid;
+                rewardGold = enemyData[1].rewardGold;
+                rewardManaStone = enemyData[1].rewardManaStone;
                 nowStageText.text = "1 - 2";
                 nowChapter = 1;
                 break;
             case 3:
+                enemyHp = enemyData[2].enemyHp;
+                enemyAtk = enemyData[2].enemyAtk;
+                enemyRapid = enemyData[2].enemyRapid;
+                rewardGold = enemyData[2].rewardGold;
+                rewardManaStone = enemyData[2].rewardManaStone;
                 nowStageText.text = "1 - 3";
                 nowChapter = 1;
                 break;
             case 4:
+                enemyHp = enemyData[3].enemyHp;
+                enemyAtk = enemyData[3].enemyAtk;
+                enemyRapid = enemyData[3].enemyRapid;
+                rewardGold = enemyData[3].rewardGold;
+                rewardManaStone = enemyData[3].rewardManaStone;
                 nowStageText.text = "1 - 4";
                 nowChapter = 1;
                 break;
             case 5:
+                enemyHp = enemyData[4].enemyHp;
+                enemyAtk = enemyData[4].enemyAtk;
+                enemyRapid = enemyData[4].enemyRapid;
+                rewardGold = enemyData[4].rewardGold;
+                rewardManaStone = enemyData[4].rewardManaStone;
                 nowStageText.text = "2 - 1";
                 nowChapter = 2;
                 break;
@@ -148,4 +179,23 @@ public class IdleEnemy : MonoBehaviour
     {
         StopAllCoroutines();
     }
+}
+
+[System.Serializable]
+public class EnemyData
+{
+    [Header ("스테이지")]
+    public int nowStage;
+    [Header ("챕터")]
+    public int nowChapter;
+    [Header ("적 체력")]
+    public int enemyHp;
+    [Header ("적 공격력")]
+    public int enemyAtk;
+    [Header ("적 공격속도 (공격 주기입니다. ex)0.1 = 초당 10회")]
+    public int enemyRapid;
+    [Header ("보상 골드")]
+    public uint rewardGold;
+    [Header ("보상 마력석")]
+    public uint rewardManaStone;
 }
