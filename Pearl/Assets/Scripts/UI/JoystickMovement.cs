9da55b2276e34ffa32d112eab528ffd8091fe67c
Park Hyunho <lumenize@ajou.ac.kr>
2023-09-14 16:54:19 +0900

    Update Joystick Movement
    조이스틱 구현 방식 변경
    기존 - 화면 전체에서 반응형 조이스틱
    
    edit ver1.0 - 설정된 좌측 패널 내부에서만 사용 가능, 패널을 벗어나는 경우 조이스틱 off
    
    edit ver2.0 - 설정된 좌측 패널 내부에서만 조이스틱 시작 가능, 드래그가 끝나는 시점에 조이스틱 사라짐

--- Pearl/Assets/Scripts/UI/JoystickMovement.cs
+++ Pearl/Assets/Scripts/UI/JoystickMovement.cs
@@ -28,12 +28,7 @@ public class JoystickMovement : MonoBehaviour
     public Vector3 joyVec;
     Vector3 stickFirstPosition;
     float stickRadius;
-    RectTransform panelRect;
 
-    private void Awake()
-    {
-        panelRect = GetComponent<RectTransform>();
-    }
     void Start()
     {
         stickRadius = bGStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
@@ -51,38 +46,20 @@ public class JoystickMovement : MonoBehaviour
         PointerEventData pointerEventData = baseEventData as PointerEventData;
         Vector3 dragPosition = pointerEventData.position;
 
-        // 패널의 크기를 가져옴
-        Vector2 panelSize = panelRect.rect.size;
+        joyVec = (dragPosition - stickFirstPosition).normalized;
+        float stickDistance = Vector3.Distance(dragPosition, stickFirstPosition);
 
-        // 패널의 경계를 화면 좌표로 변환
-        Vector3 minPanelBounds = panelRect.TransformPoint(new Vector3(-panelSize.x / 2, -panelSize.y / 2, 0));
-        Vector3 maxPanelBounds = panelRect.TransformPoint(new Vector3(panelSize.x / 2, panelSize.y / 2, 0));
-
-        // 드래그된 위치가 패널 범위 내에 있는지 확인
-        if (dragPosition.x >= minPanelBounds.x && dragPosition.x <= maxPanelBounds.x &&
-            dragPosition.y >= minPanelBounds.y && dragPosition.y <= maxPanelBounds.y)
+        if (stickDistance < stickRadius)
         {
-            // 패널 범위 내에서 드래그 중이므로 조이스틱을 업데이트
-            joyVec = (dragPosition - stickFirstPosition).normalized;
-            float stickDistance = Vector3.Distance(dragPosition, stickFirstPosition);
-
-            if (stickDistance < stickRadius)
-            {
-                smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
-            }
-            else
-            {
-                smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
-            }
+            smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
         }
         else
         {
-            // 패널 범위를 벗어났으므로 Drop 함수 호출
-            Drop();
+            smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
         }
     }
 
-    public void Drop()
+    public void OnEndDrag()
     {
         joyVec = Vector3.zero;
         bGStick.SetActive(false);
