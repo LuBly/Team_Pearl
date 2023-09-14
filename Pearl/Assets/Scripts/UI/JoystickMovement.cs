025f679ae2ef45461f11f3bed50478be860423b9
Park Hyunho <lumenize@ajou.ac.kr>
2023-09-14 14:39:24 +0900

    update JoystickMovement
    조이스틱 구현 방식 변경
    기존 - 화면 전체에서 반응형 조이스틱
    
    신규 - 설정된 좌측 패널 내부에서만 사용 가능, 패널을 벗어나는 경우 조이스틱 off

--- Pearl/Assets/Scripts/UI/JoystickMovement.cs
+++ Pearl/Assets/Scripts/UI/JoystickMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -26,10 +24,16 @@ public class JoystickMovement : MonoBehaviour
 
     public GameObject smallStick;
     public GameObject bGStick;
-    Vector3 stickFirstPosition;
-    public Vector3 joyVec;
-    float stickRadius;
     
+    public Vector3 joyVec;
+    Vector3 stickFirstPosition;
+    float stickRadius;
+    RectTransform panelRect;
+
+    private void Awake()
+    {
+        panelRect = GetComponent<RectTransform>();
+    }
     void Start()
     {
         stickRadius = bGStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
@@ -45,17 +49,36 @@ public class JoystickMovement : MonoBehaviour
     public void Drag(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
-        Vector3 DragPosition = pointerEventData.position;
-        joyVec = (DragPosition - stickFirstPosition).normalized;
-        float stickDistacne = Vector3.Distance(DragPosition, stickFirstPosition);
+        Vector3 dragPosition = pointerEventData.position;
 
-        if (stickDistacne < stickRadius)
+        // 패널의 크기를 가져옴
+        Vector2 panelSize = panelRect.rect.size;
+
+        // 패널의 경계를 화면 좌표로 변환
+        Vector3 minPanelBounds = panelRect.TransformPoint(new Vector3(-panelSize.x / 2, -panelSize.y / 2, 0));
+        Vector3 maxPanelBounds = panelRect.TransformPoint(new Vector3(panelSize.x / 2, panelSize.y / 2, 0));
+
+        // 드래그된 위치가 패널 범위 내에 있는지 확인
+        if (dragPosition.x >= minPanelBounds.x && dragPosition.x <= maxPanelBounds.x &&
+            dragPosition.y >= minPanelBounds.y && dragPosition.y <= maxPanelBounds.y)
         {
-            smallStick.transform.position = stickFirstPosition+joyVec*stickDistacne;
+            // 패널 범위 내에서 드래그 중이므로 조이스틱을 업데이트
+            joyVec = (dragPosition - stickFirstPosition).normalized;
+            float stickDistance = Vector3.Distance(dragPosition, stickFirstPosition);
+
+            if (stickDistance < stickRadius)
+            {
+                smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
+            }
+            else
+            {
+                smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
+            }
         }
         else
         {
-            smallStick.transform.position = stickFirstPosition+joyVec*stickRadius;
+            // 패널 범위를 벗어났으므로 Drop 함수 호출
+            Drop();
         }
     }
 
