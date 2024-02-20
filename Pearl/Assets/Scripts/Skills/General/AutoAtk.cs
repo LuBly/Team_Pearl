c315027cdfab6d5a2b3659f6417a6eaafc60f4a7
Park Hyunho <lumenize@ajou.ac.kr>
2024-02-20 19:02:42 +0900

    edit SkillSystem
    드론 공격스킬 모듈화작업

---
+++ Pearl/Assets/Scripts/Skills/General/AutoAtk.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAtk : Skill
+{
+    [Header("적 인식 범위")]
+    public float skillRange;
+    [Header("공격과 공격 사이 Delay")]
+    public float attackDelay;
+    [Header("사용할 BulletPrefab Idx")]
+    public int bulletIdx;
+
+    // 자동공격 때 사용할 쿨타임
+    private float curTime;
+    private Scanner scanner;
+    private void Awake()
+    {
+        scanner = GetComponent<Scanner>();
+        curTime = 0;
+        //Dev Debugging
+        transform.Find("SkillRange").GetComponent<Transform>().localScale = new Vector3(skillRange * 4, skillRange * 4, 1);
+    }
+    private void Start()
+    {
+        skillType = SkillType.autoAttack;
+        scanner.scanRange = skillRange;
+        scanner.targetLayer = enemyLayer;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+        if (curTime >= attackDelay)
+        {
+            // 스캐너에 걸리는 몬스터가 있다면 
+            if (scanner.nearestTarget != null)
+            {
+                Debug.Log("Fire");
+                DronFire(scanner.nearestTarget);
+                curTime = 0f;
+            }
+        }
+    }
+
+    private void DronFire(Transform target)
+    {
+        Vector3 targetPos = target.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+        Transform bullet = GameObject.FindWithTag("GM")
+                                     .GetComponent<GameManager>().pool
+                                     .SkillBulletGet(bulletIdx).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.down, dir);
+        bullet.GetComponent<Bullet>().Init(damage, 0, knockbackPower, 3f, dir);
+    }
+}
--- Pearl/Assets/Scripts/Skills/Skill.cs
+++ Pearl/Assets/Scripts/Skills/Skill.cs
@@ -1,19 +1,4 @@
-using System.Collections;
 using UnityEngine;
-using System;
-using TMPro;
-
-[Serializable]
-public class AutoAtk
-{
-    public Scanner scanner;
-    [Header("적 인식 범위")]
-    public float skillRange;
-    [Header("공격과 공격 사이 Delay")]
-    public float attackDelay;
-    [Header("사용할 BulletPrefab Idx")]
-    public int bulletIdx;
-}
 
 public enum SkillType
 {
@@ -25,17 +10,7 @@ public enum SkillType
 }
 public class Skill : MonoBehaviour
 {
-
-    /// skillType      : 스킬의 종류
-    /// -> 지속 공격, 단일 공격, 비네트 공격, 기본 공격 강화, etc
-    /// enableTime     : 활성화 시간
-    /// attackTime     : 피해를 주는 시간 단위
-    /// damage         : 틱 당 데미지
-    /// knockbackPower : 틱 당 넉백 정도
-    /// </summary>
     [HideInInspector][SerializeField] public SkillType skillType;
-    [HideInInspector][SerializeField] private SnipperAtk snipperAtk;
-    [HideInInspector][SerializeField] private AutoAtk autoAtk;
     [HideInInspector][SerializeField] public GameManager gameManager;
 
     // 항상 사용
@@ -43,25 +18,10 @@ public class Skill : MonoBehaviour
     public float knockbackPower;
     public LayerMask enemyLayer;
     public bool isStopFire;
-
-    // 자동공격 때 사용할 쿨타임
-    float curTime;
-
     
-
     private void Awake()
     {
         gameManager = GameObject.FindWithTag("GM").GetComponent<GameManager>();
-        switch (skillType)
-        {
-            case SkillType.autoAttack:
-                autoAtk.scanner.scanRange = autoAtk.skillRange;
-                autoAtk.scanner.targetLayer = enemyLayer;
-                curTime = 0;
-                //Dev Debugging
-                transform.Find("SkillRange").GetComponent<Transform>().localScale = new Vector3(autoAtk.skillRange * 4, autoAtk.skillRange * 4, 1);
-                break;
-        }
     }
 
     private void Start()
@@ -70,47 +30,11 @@ public class Skill : MonoBehaviour
         {
             gameManager.isStopFire = true;
         }
-        switch (skillType)
-        {
-            case SkillType.autoAttack:
-                break;
-        }
     }
 
-    private void Update()
-    {
-        switch (skillType)
-        {
-            case SkillType.autoAttack:
-                curTime += Time.deltaTime;
-                if (curTime >= autoAtk.attackDelay)
-                {
-                    // 스캐너에 걸리는 몬스터가 있다면 
-                    if (autoAtk.scanner.nearestTarget != null)
-                    {
-                        Debug.Log("Fire");
-                        DronFire(autoAtk.scanner.nearestTarget);
-                        curTime = 0f;
-                    }
-                }
-                break;
-        }
-    }
     private void OnDestroy()
     {
-        gameManager.isStopFire = false;
-    }
-    
-    public void DronFire(Transform target)
-    {
-        Vector3 targetPos = target.position;
-        Vector3 dir = targetPos - transform.position;
-        dir = dir.normalized;
-        Transform bullet = GameObject.FindWithTag("GM")
-                                     .GetComponent<GameManager>().pool
-                                     .SkillBulletGet(autoAtk.bulletIdx).transform;
-        bullet.position = transform.position;
-        bullet.rotation = Quaternion.FromToRotation(Vector3.down, dir);
-        bullet.GetComponent<Bullet>().Init(damage, 0, knockbackPower, 3f, dir);
+        if(gameManager != null)
+            gameManager.isStopFire = false;
     }
 }
