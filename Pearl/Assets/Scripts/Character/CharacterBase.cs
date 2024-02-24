d21f86b4e55d874e05130e90c0ecda7c9b693a20
Seok6704 <dnflrkcjswo@naver.com>
2024-02-24 14:23:13 +0900

    Idle Stage Remake
    Idle Stage Remake Update

--- Pearl/Assets/Scripts/Character/CharacterBase.cs
+++ Pearl/Assets/Scripts/Character/CharacterBase.cs
@@ -13,7 +13,7 @@ public class CharacterBase : MonoBehaviour
     public int atk; // 공격력
     public int gunAtk; // 총기 공격력
     public int gunProficiency; // 총기 숙련도
-    public float gunRapid; // 총기 공격속도 (공격속도 수치가 공격 간격입니다. ex 0.5 = 0.5초에 한번 공격, 1 = 1초에 한번 공격)
+    public float gunRapid; // 총기 공격속도
     public int health; // 체력
     public int critical; // 크리티컬 확률
     public int criticalDmg; // 크리티컬 데미지
