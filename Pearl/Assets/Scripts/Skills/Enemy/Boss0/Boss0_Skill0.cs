b60d734a352317b1f49a179f47fcb6d9bd143230
Park Hyunho <lumenize@ajou.ac.kr>
2024-02-26 17:14:37 +0900

    edit BossSkill0
    돌진 구현

--- Pearl/Assets/Scripts/Skills/Enemy/Boss0/Boss0_Skill0.cs
+++ Pearl/Assets/Scripts/Skills/Enemy/Boss0/Boss0_Skill0.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 /// <summary>
 /// 범위 내에 적이 들어오면 시전
 /// 보스는 멈춰선다.
@@ -16,14 +19,22 @@ public class Boss0_Skill0 : Skill
     public float rushSpeed;
     public float minTargetSearchTime, maxTargetSearchTime;
     public float minRushDelay, maxRushDelay;
+    public float minRushRatio, maxRushRatio;
+
     public Transform DirIndicatorBG;
     public Transform DirIndicator;
     public Transform Player;
+
+
     private float targetSearchTime;
     private float rushDelay;
-    private bool targetSearch;
+    private float rushRatio;
     private float curTime;
-    private Vector3 DirOrig, DirFlip;
+
+    private bool targetSearch;
+    private bool isRushStart;
+
+    private Vector3 targetPosition;
     
     private void OnEnable()
     {
@@ -31,22 +42,14 @@ public class Boss0_Skill0 : Skill
         skillType = SkillType.rushAtk;
         targetSearchTime = Random.Range(minTargetSearchTime, maxTargetSearchTime);
         rushDelay = Random.Range(minRushDelay, maxRushDelay);
+        rushRatio = Random.Range(minRushRatio, maxRushRatio);
+        transform.position = caster.transform.position + new Vector3(0, -1, 0);
+
+        DirIndicator.localScale = new Vector3(1, 0, 0);
+        DirIndicatorBG.gameObject.SetActive(true);
         targetSearch = false;
+        isRushStart = false;
         curTime = 0;
-
-        bool parentFlipped = transform.lossyScale.x < 0;
-
-        // 자식 객체의 스케일을 조정하여 뒤집힘을 방지합니다.
-        if (parentFlipped)
-        {
-            DirOrig = new Vector3(-DirIndicatorBG.transform.localScale.x, DirIndicatorBG.transform.localScale.y, DirIndicatorBG.transform.localScale.z);
-            DirFlip = new Vector3(DirIndicatorBG.transform.localScale.x, DirIndicatorBG.transform.localScale.y, DirIndicatorBG.transform.localScale.z);
-        }
-        else
-        {
-            DirOrig = new Vector3(DirIndicatorBG.transform.localScale.x, DirIndicatorBG.transform.localScale.y, DirIndicatorBG.transform.localScale.z);
-            DirFlip = new Vector3(-DirIndicatorBG.transform.localScale.x, DirIndicatorBG.transform.localScale.y, DirIndicatorBG.transform.localScale.z);
-        }
     }
 
     private void Update()
@@ -58,42 +61,58 @@ public class Boss0_Skill0 : Skill
             // 방향 벡터를 각도로 변환
             float angleToPlayer = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
             // DirIndicatorBG의 회전을 설정 Y축을 기준으로 플레이어의 방향을 바라보도록 설정
-            DirIndicatorBG.rotation = Quaternion.Euler(0f, 0f, angleToPlayer);
-
-            // 부모 객체의 스케일이 뒤집히는지 확인
-            bool parentFlipped = transform.lossyScale.x < 0;
-
-            // 자식 객체의 스케일을 조정하여 뒤집힘을 방지
-            if (parentFlipped)
-                DirIndicatorBG.localScale = DirFlip;
-            else
-                DirIndicatorBG.localScale = DirOrig;
-
-
+            transform.rotation = Quaternion.Euler(0f, 0f, angleToPlayer);
             curTime += Time.deltaTime;
 
             if (curTime >= targetSearchTime)
             {
+                targetPosition = Player.position;
                 targetSearch = true;
-                // 이제 오브젝트가 이동을 시작해야 합니다. rushDelay 시간 후에 이동 시작합니다.
-                Debug.Log("TargetFind");
-                StartRush();
+                StartCoroutine(RushCountDown());   
             }
+        }
+
+        if (isRushStart)
+        {
+            StartRush(targetPosition);
         }
     }
 
-    private void StartRush()
+    private IEnumerator RushCountDown()
     {
-        // 설정된 방향으로 오브젝트를 이동시킵니다.
-        //transform.Translate(Vector2.forward * rushSpeed * Time.deltaTime);
-        EndRush();
+        float elapsedTime = 0f;
+        float totalRushDelay = rushDelay;
+
+        while (elapsedTime < totalRushDelay)
+        {
+            float scaleFactor = Mathf.Clamp01(elapsedTime / totalRushDelay);
+            DirIndicator.localScale = new Vector3(1, scaleFactor, 0);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        DirIndicator.localScale = new Vector3(1, 1, 0);
+        isRushStart = true;
     }
 
+    private void StartRush(Vector3 targetPosition)
+    {
+        DirIndicatorBG.gameObject.SetActive(false);
+        caster.transform.position = Vector3.MoveTowards(caster.transform.position, targetPosition * rushRatio, rushSpeed * Time.deltaTime);
+
+        // 달리기가 목표 지점에 도달했다면 스킬 종료.
+        float dis = Vector3.Distance(targetPosition * rushRatio, caster.transform.position);
+        
+        if (dis < 1.05f)
+        {
+            EndRush();
+        }
+    }
     private void EndRush()
     {
-        this.gameObject.SetActive(false);
         enemySkill.isSkillActive = false;
         EndSkill();
         StopAllCoroutines();
+        this.gameObject.SetActive(false);
     }
 }
